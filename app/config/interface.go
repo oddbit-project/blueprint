ce65b7b7b6779640f3a6d9fa0463c1151138a2de
Joao Pinheiro <jpinheiro@dognaedis.com>
2020-12-13 17:38:18 +0000

    config: json provider

---
+++ app/config/interface.go
@@ -0,0 +1,19 @@
+package config
+
+import "github.com/oddbit-project/blueprint/utils"
+
+const (
+	ErrNoKey = utils.Error("Config key does not exist")
+)
+
+type ConfigInterface interface {
+	GetKey(key string, dest interface{}) error
+	GetStringKey(key string) (string, error)
+	GetBoolKey(key string) (bool, error)
+	GetIntKey(key string) (int,error)
+	GetFloat64Key(key string) (float64, error)
+	GetSliceKey(key, separator string) ([]string, error)
+	GetConfigNode(key string) (ConfigInterface, error)
+	KeyExists(key string) bool
+	KeyListExists(keys []string) bool
+}
