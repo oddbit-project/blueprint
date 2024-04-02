2340249ac071e0cdf06e21f64b07882b7050f261
jpinheiro <jpinheiro@blackshield.pt>
2024-04-02 11:10:03 +0100

    dbclient rework

--- db/client.go
+++ db/client.go
@@ -18,24 +18,5 @@ type ClientInterface interface {
 type Client struct {
 	ClientInterface
 	Conn *sqlx.DB
-}
-
-func (c *Client) Connect() error {
-
-	return ErrAbstractMethod
-}
-func (c *Client) GetClient() *sqlx.DB {
-	return c.Conn
-}
-
-func (c *Client) IsConnected() bool {
-	return c.Conn != nil
-}
-
-func (c *Client) Disconnect() {
-	if c.Conn == nil {
-		return
-	}
-	_ = c.Conn.Close()
-	c.Conn = nil
+	Dsn  string
 }
