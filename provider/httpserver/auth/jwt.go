da7f73a47749985d3084f4e1a1a3930987e6a0cf
jpinheiro <jpinheiro@blackshield.pt>
2025-07-05 16:26:51 +0100

    jwt provider auth

---
+++ provider/httpserver/auth/jwt.go
@@ -0,0 +1,45 @@
+package auth
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/oddbit-project/blueprint/provider/jwtprovider"
+	"net/http"
+)
+
+const (
+	ErrMissingAuthHeader = "missing or invalid Authorization header"
+	ContextJwtClaims     = "jwtClaims"
+)
+
+type authJWT struct {
+	parser jwtprovider.JWTParser
+}
+
+func NewAuthJWT(p jwtprovider.JWTParser) Provider {
+	return &authJWT{
+		parser: p,
+	}
+}
+
+func (a *authJWT) CanAccess(c *gin.Context) bool {
+	authHeader := c.GetHeader("Authorization")
+	if len(authHeader) < 7 || authHeader[:7] != "Bearer " {
+		c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": ErrMissingAuthHeader})
+		return false
+	}
+
+	claims, err := a.parser.ParseToken(authHeader[7:])
+	if err != nil || len(claims) == 0 {
+		c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "invalid token"})
+		return false
+	}
+
+	if err = a.parser.ValidateClaims(claims); err != nil {
+		c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": err.Error()})
+		return false
+	}
+
+	c.Set(ContextJwtClaims, claims)
+	c.Next()
+	return true
+}
